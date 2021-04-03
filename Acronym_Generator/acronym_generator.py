86d3bf910d4d36c9800a98e54e0b3002c128f1aa
Srushti Shimpi <55121414+srushtishimpi@users.noreply.github.com>
2021-04-04 05:12:42 +0530

    Update acronym_generator.py

--- Acronym_Generator/acronym_generator.py
+++ Acronym_Generator/acronym_generator.py
@@ -1,1 +1,8 @@
+userinput = str(input("Enter a Phrase: ")) # take the input pharse from user
+t = userinput.split() # Use split() for splitting sentense
+s = " "  
+for i in t:w  # Run a loop for splitting every word in sentence and making first letter of each word capital
+        s = s+str(i[0]).upper()
+    
+print("Acronym for"+userinput+ " is"+s+".") # Print the acronym of the phrase
 
